6ba50a1d764fd2bc5810d9653cb7f23b8b69fc1b
whysosergious <sergio.stankevich@gmail.com>
2024-06-04 05:48:12 +0200

    install-script

--- lua/pocket/lazy.lua
+++ lua/pocket/lazy.lua
@@ -14,6 +14,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   { import = "pocket.plugins" },
   { import = "pocket.plugins.lsp" },
+  { import = "pocket.plugins.lsp.servers" },
   {
     "folke/tokyonight.nvim",
     lazy = false,
--- lua/pocket/plugins/treesitter.lua
+++ lua/pocket/plugins/treesitter.lua
@@ -1,18 +1,18 @@
 return {
 
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPre', 'BufNewFile' },
-  build = ':TSUpdate',
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPre", "BufNewFile" },
+  build = ":TSUpdate",
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects',
-    'nvim-treesitter/nvim-treesitter-context',
-    'nvim-treesitter/nvim-treesitter-refactor',
-    'windwp/nvim-ts-autotag',
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "nvim-treesitter/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-refactor",
+    "windwp/nvim-ts-autotag",
     -- 'nushell/tree-sitter-nu',
   },
   config = function()
     -- import nvim-treesitter plugin
-    local treesitter = require 'nvim-treesitter.configs'
+    local treesitter = require "nvim-treesitter.configs"
 
     -- configure treesitter
     treesitter.setup {
@@ -30,40 +30,41 @@ return {
       },
       -- ensure these language parsers are installed
       ensure_installed = {
-        'rust',
-        'json',
-        'jsonc',
-        'javascript',
-        'typescript',
-        'toml',
-        'tsx',
-        'html',
-        'css',
-        'markdown',
-        'markdown_inline',
-        'lua',
-        'vim',
-        'gitignore',
-        'query',
+        "rust",
+        "json",
+        "jsonc",
+        "javascript",
+        "typescript",
+        "toml",
+        "tsx",
+        "html",
+        "css",
+        "markdown",
+        "markdown_inline",
+        "lua",
+        "vim",
+        "gitignore",
+        "query",
+        "nu",
       },
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = '<C-space>',
-          node_incremental = '<C-.>',
-          scope_incremental = '<C-space>',
-          node_decremental = '<C-,>',
+          init_selection = "<C-space>",
+          node_incremental = "<C-.>",
+          scope_incremental = "<C-space>",
+          node_decremental = "<C-,>",
         },
       },
 
-      require('treesitter-context').setup {
+      require("treesitter-context").setup {
         enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
         max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
         min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
         line_numbers = true,
         multiline_threshold = 20, -- Maximum number of lines to show for a single context
-        trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-        mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
         -- Separator between context and content. Should be a single character string, like '-'.
         -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
         separator = nil,
